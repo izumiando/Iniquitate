193dd262bc56bdcee637a922468d4411f1e95487
Izumi Ando <andoizumita@gmail.com>
2025-03-05 16:24:40 -0500

    removed kmeans (faiss) code

--- workflow/scripts/python/integrate_data_helical.py
+++ workflow/scripts/python/integrate_data_helical.py
@@ -8,7 +8,7 @@ import scanpy as sc
 import anndata as ann
 import numpy as np
 
-from utils import IntegrationHelical, downsample, faiss_kmeans
+from utils import IntegrationHelical, downsample
 
 
 def none_or_str(value):
@@ -96,63 +96,6 @@ def main(h5ad_dir, save_loc, ds_celltypes, ds_proportions, num_batches, seed):
     ])
     integrated_concat.obs_names = range(len(integrated_concat.obs_names))
     integrated_concat.obs_names_make_unique()
-    
-    # Add placeholder in entire obs dataframe for kmeans clustering
-    integrated_concat.obs["kmeans_faiss"] = np.zeros(len(integrated_concat.obs_names))
-    
-    # Perform kmeans clustering on integrated data 
-    # Define method subsets and iterate over them until the same number of k clusters is found
-    k = 10
-    k_initial = k # Integers are immutable 
-    methods = ["uce", "scgpt", "geneformer"]
-    method_kmeans_adatas = []
-    i = 0
-    while i < len(methods):
-        # Create a copy of adata to avoid overwriting the original
-        adata_copy = integrated_concat.copy()
-        
-        # Define method subset
-        adata_subset = adata_copy[adata_copy.obs["integration_method"] == methods[i]]
-        
-        # Perform HVG selection on raw (unnormalized, unlogged) data
-        adata_subset.X = adata_subset.layers["raw"]
-        sc.pp.normalize_total(
-            adata_subset,
-            target_sum = 1e4
-        )
-        sc.pp.log1p(adata_subset)
-        sc.pp.highly_variable_genes(
-            adata_subset,
-            n_top_genes = 2500,
-            flavor = "seurat"
-        )
-        
-        # Perform faiss kmeans clustering
-        adata_subset, k_method = faiss_kmeans(adata_subset, k)
-        
-        # Test concordance of k values and either append or reset
-        if k_method != k:
-            k = k_method
-            i = 0 
-            method_kmeans_adatas.clear()
-            continue 
-        else:
-            i += 1
-            method_kmeans_adatas.append(adata_subset)
-    
-    # Append kmeans cluster info to integrated data
-    for method, method_kmeans_adata in zip(methods, method_kmeans_adatas):
-        method_kmeans_clusters = method_kmeans_adata.obs["kmeans_faiss"].__array__().astype('str')
-        integrated_concat.obs.loc[
-            integrated_concat.obs["integration_method"] == method,
-            "kmeans_faiss"
-        ] = method_kmeans_clusters
-    
-    # Append information about kmeans faiss clusters to .uns of adata_concat
-    integrated_concat.uns["kmeans_stats"] = {
-        "kmeans_initial_k": k_initial,
-        "kmeans_final_k": k
-    }
 
     # If downsampled celltypes and batches are of array length greater than one, combine them 
     if len(batches_ds) > 1:
