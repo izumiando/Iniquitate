c1a293d3898bfe6af1893767cc88cdb6e4a669a0
Izumi Ando <andoizumita@gmail.com>
2025-07-23 18:01:07 -0400

    efforts to make jobs run faster & not fail due to memory allocations

--- workflow/src/python/utils_helical/integrate_helical.py
+++ workflow/src/python/utils_helical/integrate_helical.py
@@ -50,7 +50,7 @@ class IntegrationHelical:
     def uce_integrate(self):
         print("Performing UCE integration.." + "\n")
         auce = self.adata.copy()
-        configurer_uce = UCEConfig(model_name="33l_8ep_1024t_1280", device="cuda")
+        configurer_uce = UCEConfig(model_name="33l_8ep_1024t_1280", batch_size=16, device="cuda")
         uce = UCE(configurer=configurer_uce)
         data_loader_uce = uce.process_data(auce)
         embeddings_uce = uce.get_embeddings(data_loader_uce)
