efaa829afd102136bb7db0955452b3037c11d87e
Izumi Ando <andoizumita@gmail.com>
2025-07-28 13:31:48 -0400

    mod methods list

--- workflow/scripts/python/dge_concordance_full.py
+++ workflow/scripts/python/dge_concordance_full.py
@@ -43,7 +43,7 @@ def main(h5ad_loc, save_loc, dataset_name, rep):
     else:
         # Subset adatas based on method for integration and store lognorm counts in raw
         # attribute for diffexp testing
-        methods = ["harmony", "scvi", "scanorama", "seurat", "liger"]
+        methods = ["harmony", "scvi", "uce", "scgpt", "geneformer"]
         method_adatas = []
         for method in methods:
             adata_copy = adata.copy()
